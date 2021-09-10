a3298c6341bd817fb9f824677e79c4dfee5c6892
ArtusU <artusrock@hotmail.com>
2021-09-10 12:36:39 +0100

    first commit

---
+++ one_buy/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from one_buy.users.models import User
+from one_buy.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
