021a220b65f4f37126feddf31d137400f4a9416e
ArtusU <artusrock@hotmail.com>
2021-09-11 12:09:02 +0100

    payment.tsx

--- one_buy/frontend/src/App.tsx
+++ one_buy/frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Payment } from './components/Payment';
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { Toaster } from "react-hot-toast";
+import { Payment } from './components/Payment'
 
-function App() {
+export default function App() {
   return (
-      <PayPalScriptProvider options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID }}>
-        <Payment />
-      </PayPalScriptProvider>
+    <PayPalScriptProvider options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID }}>
+      <Toaster position="top-center" />
+      <Payment />
+    </PayPalScriptProvider>
   );
 }
-
-export default App;
