5d74a691f78f162eb6b16d9c3a2049043c36b0b0
ArtusU <artusrock@hotmail.com>
2021-09-11 12:51:00 +0100

    ProcessWebhookView

--- config/urls.py
+++ config/urls.py
@@ -5,12 +5,14 @@ from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
+from one_buy.payments.views import ProcessWebhookView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
+    path('webhooks/paypal/', ProcessWebhookView.as_view()),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
